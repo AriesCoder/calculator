4b378f7b9c2367fd6099888130b3df8e09531774
Trang Lam <ms.tranglam@gmail.com>
2022-01-18 16:01:55 -0800

    Addition implementation working

--- calc.js
+++ calc.js
@@ -14,6 +14,26 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	console.log('num1', num1);
+	console.log('num2', num2);
+
+	if (mathSymbol === "√"){
+		console.log("Square root of num1: ", Math.sqrt(num1))
+		console.log("Square root of num2: ", Math.sqrt(num2))
+	}
+	else if (mathSymbol === "-"){
+		console.log("The result of subtraction: ", num1-num2)
+	}
+	else if (mathSymbol === "/"){
+		console.log("The result of division: ", num1/num2)
+	}
+	else if (mathSymbol === "*"){
+		console.log("The result of multiplication: ", num1*num2)
+	}
+	else{
+		console.log("Please input (√ - / *) only")
+	}
+
 
 	// This line closes the connection to the command line interface.
 	reader.close()
